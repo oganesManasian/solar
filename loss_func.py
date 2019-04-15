3c0fedc98a846d9be0e445f43d50d70dec87e2e9
Oganes Manasian <oganesman8@mail.ru>
2019-04-15 11:44:52 +0300

    Added penalty for speed constraints Added factory for creating penalty functions Deleted deprecated code

--- loss_func.py
+++ loss_func.py
@@ -1,4 +1,3 @@
-from functools import reduce
 from energy_manager import compute_energy_levels, get_energy_level_in_percents
 from track import Track
 
@@ -13,45 +12,47 @@ def loss_func(section_speeds: list, track: Track):
 
     energy_levels = compute_energy_levels(track, section_speeds)
     energy_level_penalty = compute_energy_level_penalty(energy_levels)
-    # print("Loss: Only time: %5.2f Only penalty: %5.2f" % (loss, energy_level_penalty))
-    loss += energy_level_penalty  # TODO Tune: add coefficient
+    speed_penalty = compute_speed_penalty(section_speeds)
+    # print("Loss: Only time: %5.2f Only penalty: %5.2f" % (loss, energy_level_penalty + speed_penalty))
+    loss += energy_level_penalty + speed_penalty
     return loss
 
 
+def compute_speed_penalty(speeds: list):
+    penalty_func = box_penalty_func_factory(0, 25)
+    return sum([penalty_func(x) for x in speeds])
+
+
 def compute_energy_level_penalty(energy_levels: list):
+    penalty_func = box_penalty_func_factory(0, 100)
+    energy_levels_in_percents = list(map(get_energy_level_in_percents, energy_levels))
+    # Starting from 1 due to full charge of battery at beginning of race
+    return sum([penalty_func(x) for x in energy_levels_in_percents[1:]])
+
+
+def box_penalty_func_factory(a, b):
+    """Creates continuous penalty function for box constraints: a < x < b"""
+    width = b - a
+    shift = a / (width / 2) + 1
+
+    def inner(x, koef=20, deg=30):
+        return koef * (1 / (width / 2) * x - shift) ** deg
+    return inner
+
+
+def compute_energy_level_penalty_old(energy_levels: list):
     """Computes penalty"""
     energy_levels_in_percents = list(map(get_energy_level_in_percents, energy_levels))
     # Starting from 1 due to full charge of battery at beginning of race
     return sum([penalty_func_1(x) for x in energy_levels_in_percents[1:]])
 
 
-def compute_energy_level_penalty_fast(energy_levels: list):
-    """Computing penalties with discontinuous penalty function
-        Works faster than compute_energy_level_penalty"""
-    energy_levels_in_percents = list(map(get_energy_level_in_percents, energy_levels))
-
-    penalty_sum = 0.0
-
-    below_level = list(filter(lambda x: x < 0, energy_levels_in_percents))
-    below_level_penalty = list(map(lambda x: abs(x) ** 3, below_level))
-    penalty_sum += reduce(lambda x, y: x + y, below_level_penalty, 0)
-
-    above_level = list(filter(lambda x: x > 100, energy_levels_in_percents))
-    above_level_penalty = list(map(lambda x: abs(x - 100) ** 3, above_level))
-    penalty_sum += reduce(lambda x, y: x + y, above_level_penalty, 0)
-    return penalty_sum
+def penalty_func_1(x, koef=20, deg=30):
+    """Continuous penalty function for box constraints: 0 < x < 100"""
+    return koef * (1 / 50 * x - 1) ** deg
 
 
-def penalty_func_1(x):
-    """Continuous function for computing penalties for violation of task conditions"""
-    return 10 * (1 / 50 * x - 1) ** 20
+def penalty_func_2(x, deg=3):
+    """Discontinuous penalty function for box constraints: 0 < x < 100"""
+    return max(max(0, -x), max(0, x - 100)) ** deg
 
-
-def penalty_func_2(x):
-    """Discontinuous function for computing penalties for violation of task conditions"""
-    if x < 0:
-        return abs(x) ** 3
-    elif x > 100:
-        return abs(x - 100 ** 3)
-    else:
-        return 0
